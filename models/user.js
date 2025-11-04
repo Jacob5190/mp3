cbbabc58fd2b9654abc492d77617a92f9a54bb9b
Jacob5190 <ywang32@scu.edu>
2025-11-03 19:54:01 -0600

    upload

--- models/user.js
+++ models/user.js
@@ -2,9 +2,29 @@
 var mongoose = require('mongoose');
 
 // Define our user schema
-var UserSchema = new mongoose.Schema({
-    name: String
-});
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User name is required'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'User email is required'],
+    trim: true,
+    lowercase: true,
+    unique: true
+  },
+  pendingTasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    default: []
+  }],
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+}, { versionKey: false });
 
 // Export the Mongoose model
 module.exports = mongoose.model('User', UserSchema);
